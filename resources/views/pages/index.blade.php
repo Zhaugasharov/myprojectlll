19042be90b0c230574e8df2ad6eabcd7f2f993c1
berik <gh_ost@list.ru>
2018-09-07 21:55:40 +0600

    template

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+@section('title') Main page @endsection
+@section('keywords')@endsection
+@section('description')@endsection
+@section('css')@endsection
+
+@section('content')
+
+
+
+
+
+@endsection
+@section('js')@endsection
